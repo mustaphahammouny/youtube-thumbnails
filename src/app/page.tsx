b0d1c8b4cb0e06651babb8f75e1fa5333492a17e
mustapha <mhammouny@rc2k.fr>
2023-09-04 23:28:58 +0100

    fix some bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import Thumbnail from "@/types/Thumbnail";
 import axios from "axios";
 import FileSaver from "file-saver";
+import Image from "next/image";
 import { useState } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { ToastContainer, toast } from "react-toastify";
@@ -84,12 +85,12 @@ export default function Home() {
               Youtube Thumbnail Downloader
             </h2>
             <p className="mt-4 text-lg leading-8 text-gray-300">
-              Elevate your content effortlessly with our free thumbnail image
-              downloader. Simply paste your YouTube or Vimeo video URL, click,
-              and access high-quality thumbnails in various resolutions – all
-              for free. Ideal for content creators and marketers looking for
-              captivating visuals. Say goodbye to the search and hello to
-              impressive, versatile thumbnails!
+              Take your YouTube channel to the next level by easily downloading
+              high-quality thumbnails in various resolutions. Our user-friendly
+              application lets you gain inspiration from other creators&apos;
+              thumbnails, perfect for spicing up your own videos. Just paste the
+              video URL, click &apos;Download Thumbnails&apos; and elevate your
+              content with eye-catching visuals tailored to your needs.
             </p>
             <div className="mt-6 flex m-auto">
               <input
@@ -105,21 +106,26 @@ export default function Home() {
                 className="flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
                 onClick={() => getYouTubeThumbnail(url)}
               >
-                Download Thumbnails
+                Get Thumbnails
               </button>
             </div>
             {thumbnails.length > 0 && (
               <div className="flex flex-col items-center mt-6">
-                <h2 className="text-3xl font-bold text-white">
-                  Thumbnails
-                </h2>
+                <h2 className="text-3xl font-bold text-white">Thumbnails</h2>
                 <div className="mt-6">
                   {thumbnails.map((thumbnail, index) => (
                     <div
                       key={index}
                       className="flex flex-col items-center justify-center mt-6"
                     >
-                      <img src={thumbnail.url} alt={`Thumbnail ${index + 1}`} />
+                      <Image
+                        src={thumbnail.url}
+                        alt={`Thumbnail ${thumbnail.resolution}`}
+                        width={0}
+                        height={0}
+                        sizes="100vw"
+                        style={{ width: "auto", height: "auto" }}
+                      />
 
                       <div className="inline-flex" role="group">
                         <p className="my-4 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm">
@@ -148,66 +154,79 @@ export default function Home() {
 
             <div className="mt-6">
               <h2 className="mt-4 text-3xl font-bold text-white">
-                Why Use Our YouTube Thumbnail Downloader?
+                Get High-Quality YouTube Thumbnail Images for Free
               </h2>
               <p className="mt-4 text-lg leading-8 text-gray-300">
-                Our YouTube Thumbnail Downloader is a valuable tool for
-                individuals and professionals. You can utilize these downloaded
-                thumbnails in presentations, animations, or various creative
-                projects, enhancing your content's visual appeal.
+                This website allows you to easily download thumbnail images from
+                YouTube videos in high resolution. It supports Full HD (1080p),
+                HD (720p), SD (480p), and small sizes.
               </p>
               <h2 className="mt-4 text-3xl font-bold text-white">
-                How to Use Our YouTube Thumbnail Downloader?
+                Benefits of Downloading YouTube Thumbnails
               </h2>
               <p className="mt-4 text-lg leading-8 text-gray-300">
-                1) Copy the URL of the YouTube video whose thumbnail you desire.
-              </p>
-              <p className="mt-4 text-lg leading-8 text-gray-300">
-                2) Paste the URL into our input box.
-              </p>
-              <p className="mt-4 text-lg leading-8 text-gray-300">
-                3) Our tool automatically generates thumbnails in different
-                sizes.
-              </p>
-              <p className="mt-4 text-lg leading-8 text-gray-300">
-                4) Click the 'Thumbnail Download' button to save it to your
-                device.
-              </p>
-              <p className="mt-4 text-lg leading-8 text-gray-300">
-                While downloading YouTube thumbnails is legal, please be aware
-                that both thumbnails and videos are copyrighted. Always seek
-                proper permissions before reusing them.
-              </p>
-
-              <h2 className="mt-4 text-3xl font-bold text-white">
-                Compatibility and Copyright Considerations:
-              </h2>
-              <p className="mt-4 text-lg leading-8 text-gray-300">
-                Our YouTube Thumbnail Downloader works seamlessly on most
-                devices, except for iPhones, due to iOS restrictions. For
-                jailbroken iPhones, compatibility isn't an issue. It performs
-                excellently on Android devices, laptops, and desktops.
-              </p>
-              <p className="mt-4 text-lg leading-8 text-gray-300">
-                Regarding copyright, remember that downloaded YouTube
-                screenshots are copyrighted by the video's owner. Seek
-                permission for any reuse, especially on YouTube. Outside of
-                YouTube, using them for website logos or creative work typically
-                poses no issues.
+                People use this tool to get thumbnails from YouTube videos for
+                presentations, animation projects, and other uses. The
+                high-quality images can enhance your work.
               </p>
               <h2 className="mt-4 text-3xl font-bold text-white">
-                SEO-Friendly Usage:
+                How to Use the YouTube Thumbnail Downloader
               </h2>
               <p className="mt-4 text-lg leading-8 text-gray-300">
-                It's essential to note that reusing YouTube thumbnails as they
-                are may not be SEO-friendly, as Google indexes most thumbnails.
-                To improve SEO, consider adding unique effects using software
-                like Photoshop to make your thumbnails distinct and search
-                engine-friendly.
+                It&apos;s simple to download YouTube thumbnails:
               </p>
               <p className="mt-4 text-lg leading-8 text-gray-300">
-                Elevate your YouTube content with captivating thumbnails
-                effortlessly. Try our YouTube Thumbnail Downloader today!"
+                1) Copy the link to the YouTube video with the thumbnail you
+                want.
+              </p>
+              <p className="mt-4 text-lg leading-8 text-gray-300">
+                2) Paste the link in the input box. Then click &apos;Download
+                Thumbnails&apos; to generate different sized thumbnails.
+              </p>
+              <p className="mt-4 text-lg leading-8 text-gray-300">
+                3) Click the &apos;Download Image&apos; button. The images will be
+                downloaded to your device.
+              </p>
+              <p className="mt-4 text-lg leading-8 text-gray-300">
+                On Android, you need to save the images. On iPhone, you&apos;ll
+                need a jailbroken device to save them. It works on all devices
+                and desktop browsers except un-jailbroken iPhones.
+              </p>
+              <h2 className="mt-4 text-3xl font-bold text-white">
+                Copyright and Reuse Considerations
+              </h2>
+              <p className="mt-4 text-lg leading-8 text-gray-300">
+                The thumbnails are copyrighted by the video owners. To reuse
+                them, you need permission, especially for YouTube videos. For
+                other uses like websites and logos, reusing thumbnails likely
+                won&apos;t lead to DMCA complaints.
+              </p>
+              <h2 className="mt-4 text-3xl font-bold text-white">
+                Optimizing Reused Thumbnails for SEO
+              </h2>
+              <p className="mt-4 text-lg leading-8 text-gray-300">
+                Simply reusing YouTube thumbnails is not optimal for SEO since
+                Google indexes the originals. To make reused thumbnails
+                SEO-friendly, modify them with Photoshop or other software. This
+                makes them unique again for search engines.
+              </p>
+              <h2 className="mt-4 text-3xl font-bold text-white">
+                Key Takeaways
+              </h2>
+              <p className="mt-4 text-lg leading-8 text-gray-300">
+                - Easily download high-quality YouTube thumbnails for free.
+              </p>
+              <p className="mt-4 text-lg leading-8 text-gray-300">
+                - Useful for presentations, projects, and more.
+              </p>
+              <p className="mt-4 text-lg leading-8 text-gray-300">
+                - Simple process to paste a link and download images.
+              </p>
+              <p className="mt-4 text-lg leading-8 text-gray-300">
+                - Requires permission for reuse on YouTube.
+              </p>
+              <p className="mt-4 text-lg leading-8 text-gray-300">
+                - Modify thumbnails to make them SEO-friendly.
               </p>
             </div>
           </div>
